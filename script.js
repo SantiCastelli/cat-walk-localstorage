bc74adcb865ab5b8105eaa894800037dc680828a
Santiago Castelli <castellist@gmail.com>
2022-03-24 13:21:57 +0100

    terminado

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // Estado de la APP
 let moveCat = false;
 let pixelsMove = 10;
-let volume = 0.5
+let volume = 0.5;
 let walkForwards = true;
+
+
+// Gestionar localStorage y sobreescribir las variables de estado que queramos
+// JSON.parse va a convertir un string a un tipo de datos que entienda JavaScript; no solamente 'stirngs'
+
+let moveCatLocalStorage = JSON.parse(localStorage.moveCat);
+
+if (localStorage.pixelsMove) {
+    pixelsMove = JSON.parse(localStorage.pixelsMove);
+    document.forms["catConf"].elements["velocidad"].value = pixelsMove;
+}
 
 // Variables globales
 const img = document.querySelector('img');
@@ -12,6 +23,11 @@ img.style.left = '0px';
 const audio = new Audio('./lambada.mp3');
 audio.volume = volume;
 
+if (localStorage.moveCat) {
+    moveCat = JSON.parse(localStorage.moveCat);
+    audio.play();
+
+}
 // escuchar evento submit de formulario
 document.querySelector("form").addEventListener("submit", procesarValoresFormulario);
 
@@ -22,6 +38,16 @@ document.querySelector("form").addEventListener("submit", procesarValoresFormula
 // 2. Actualizar la variable de estado 'moveCat' a true
 // 3. Ejecutar el método .play del objeto 'audio'
 
+let bailar = document.querySelector("#bailar");
+bailar.addEventListener("click", function(){
+    moveCat = true;
+    audio.play();
+
+ // Actualizar el localStorage para indicar que la próxima vez que carguemos la App el gato debería moverse
+    
+    localStorage.moveCat = true;
+    
+});
 
 
 // evento click al botón Parar!
@@ -30,6 +56,14 @@ document.querySelector("form").addEventListener("submit", procesarValoresFormula
 // 1. Asociar el evento "click" al botón "Parar"
 // 2. Actualizar la variable de estado 'moveCat' a false
 // 3. Ejecutar el método .pause del objeto 'audio'
+
+let parar = document.querySelector("#parar");
+
+parar.addEventListener("click", function(){
+    moveCat = false;
+    audio.pause();
+    localStorage.moveCat = false;
+});
 
 
 function catWalk() {
@@ -49,9 +83,9 @@ function catWalk() {
     // Ejercicio 4
 
     if (walkForwards) {
-        img.style.left = (currentLeft + 10) + 'px';
+        img.style.left = (currentLeft + pixelsMove) + 'px';
     } else {
-        img.style.left = (currentLeft - 10) + 'px';
+        img.style.left = (currentLeft - pixelsMove) + 'px';
     }
 }
 
@@ -65,9 +99,19 @@ function procesarValoresFormulario(event) {
     const velocidad = document.forms["catConf"].elements["velocidad"].value;
     console.log("nueva velocidad", velocidad);
 
+    // De los formularios siempre se recupera un stirng. Hay que transformarlo en number 
+
+    pixelsMove = parseInt(velocidad);
+    // pixelsMove = +velocidad;
+
     // Ejercicio 4
+    
+    localStorage.pixelsMove = pixelsMove;
 
     // Ejercicio 5
+    volume = document.forms["catConf"].elements["volumen"].value;
+    audio.volume = volume;
+    localStorage.volume = volume;
 
 }
 
